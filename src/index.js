1188e69e72f1d1ac23086a715e040f1afaf78055
Ricardo Proença <ricardo.proenca@enear.co>
2018-07-30 23:16:12 +0100

    prettier + eslint config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import SlackBots from "slackbots";
+import SlackBots from 'slackbots';
 
 /**
  * To be able to extend slackbots, we will need to add to .babelrc
@@ -6,15 +6,15 @@ import SlackBots from "slackbots";
  */
 
 const bot = new SlackBots({
-  token: "xoxb-396722210992-397582760805-O1lt2o9E7cicbcGC4pEHpMsI",
-  name: "chuck_norris_bot"
+  token: 'xoxb-396722210992-397582760805-O1lt2o9E7cicbcGC4pEHpMsI',
+  name: 'chuck_norris_bot',
 });
 
 // TODO: use node.js json database to save is first run
 
-bot.on("start", () => {
-  console.log("bot :: start");
-  const channel = "general";
-  const params = { icon_emoji: ":cat:" };
+bot.on('start', () => {
+  console.log('bot :: start');
+  const channel = 'general';
+  const params = { icon_emoji: ':cat:' };
   bot.postMessageToChannel(channel, `Hello ${channel}!`, params);
 });
